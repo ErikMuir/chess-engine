04bcaaac64974bf5fd1b1f5cbd8d60d0a9835582
Erik Muir <erikdmuir@gmail.com>
2021-05-11 16:46:33 -0500

    start building engine

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,8 @@ let _ctx;
 let _whitePieces, _blackPieces;
 let _fromSquare, _toSquare;
 let _dragX, _dragY, _dragPiece;
+let _directionOffsets = [8, -8, -1, 1, 7, -7, 9, -9];
+let _numSquaresToEdge;
 
 // ------------------------------------
 // classes
@@ -234,6 +236,30 @@ function initBoard() {
   _board.loadFen(startPosition);
 }
 
+function initEngine() {
+  for (let file = 0; file < 8; file++) {
+    for (let rank = 0; rank < 8; rank++) {
+      const numNorth = 7 - rank;
+      const numSouth = rank;
+      const numWest = file;
+      const numEast = 7 - file;
+
+      const squareIndex = rank * 8 + file;
+
+      _numSquaresToEdge[squareIndex] = [
+        numNorth,
+        numSouth,
+        numWest,
+        numEast,
+        Math.min(numNorth, numWest),
+        Math.min(numSouth, numEast),
+        Math.min(numNorth, numEast),
+        Math.min(numSouth, numWest),
+      ];
+    }
+  }
+}
+
 function draw() {
   _board.draw();
   if (_dragPiece) {
@@ -288,5 +314,6 @@ function onLoad() {
   initCanvas();
   initPieces();
   initBoard();
+  initEngine();
   return setInterval(draw, 10);
 }
