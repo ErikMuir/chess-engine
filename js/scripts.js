d7461cf399fb7d334bbf17ae657098b105715f2f
Erik Muir <erikdmuir@gmail.com>
2021-05-17 20:57:31 -0500

    finish game class

--- js/scripts.js
+++ js/scripts.js
@@ -1,126 +1,231 @@
-// ------------------------------------
-// globals
-// ------------------------------------
-const _startPosition = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
-const _directionOffsets = [8, -8, -1, 1, 7, -7, 9, -9];
-const _numSquaresToEdge = new Array(64);
-const _lightColor = '#f1d9c0';
-const _darkColor = '#a97a65';
-const _activeOverlay = '#00cc00';
-const _previousOverlay = '#cccc00';
-const _overlayOpacity = 0.4;
-const _squareSize = 80;
-let _board;
-let _canvas;
-let _ctx;
-let _whitePieces, _blackPieces;
-let _prevMoveSquares = [];
-let _activeSquare, _hoverSquare;
-let _hoverX, _hoverY, _dragPiece;
+// By en:User:Cburnett - Own work  This W3C-unspecified vector image was created with Inkscape ., CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=1499803
 
-// ------------------------------------
-// page load
-// ------------------------------------
 window.onload = () => {
-  initCanvas();
-  initPieces();
-  initBoard();
-  initEngine();
-  return setInterval(draw, 10);
+  const game = new Game();
+  game.init();
 }
 
-// ------------------------------------
-// initialization functions
-// ------------------------------------
-function initCanvas() {
-  _canvas = document.getElementById('canvas');
-  _canvas.width = _squareSize * 8;
-  _canvas.height = _squareSize * 8;
-  _canvas.onmousedown = onMouseDown;
-  _canvas.onmouseup = onMouseUp;
-  _canvas.onmousemove = onMouseMove;
-  _canvas.onmouseout = onMouseOut;
-  _ctx = canvas.getContext('2d');
-}
-
-function initPieces() {
-  _whitePieces = {
-    King: document.getElementById('white-king'),
-    Pawn: document.getElementById('white-pawn'),
-    Knight: document.getElementById('white-knight'),
-    Bishop: document.getElementById('white-bishop'),
-    Rook: document.getElementById('white-rook'),
-    Queen: document.getElementById('white-queen'),
-  };
-  _blackPieces = {
-    King: document.getElementById('black-king'),
-    Pawn: document.getElementById('black-pawn'),
-    Knight: document.getElementById('black-knight'),
-    Bishop: document.getElementById('black-bishop'),
-    Rook: document.getElementById('black-rook'),
-    Queen: document.getElementById('black-queen'),
-  };
-}
-
-function initBoard() {
-  _board = new Board(_startPosition);
-}
-
-function initEngine() {
-  for (let file = 0; file < 8; file++) {
-    for (let rank = 0; rank < 8; rank++) {
-      const numNorth = 7 - rank;
-      const numSouth = rank;
-      const numWest = file;
-      const numEast = 7 - file;
-
-      const squareIndex = rank * 8 + file;
-
-      _numSquaresToEdge[squareIndex] = [
-        numNorth,
-        numSouth,
-        numWest,
-        numEast,
-        Math.min(numNorth, numWest),
-        Math.min(numSouth, numEast),
-        Math.min(numNorth, numEast),
-        Math.min(numSouth, numWest),
-      ];
-    }
-  }
-}
-
-// ------------------------------------
-// classes
-// ------------------------------------
 class Game {
   constructor() {
-    this.board = new Board();
+    this.board = null;
+    this.ctx = null;
+    this.whitePieces = {};
+    this.blackPieces = {};
+    this.startPosition = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+    this.squareSize = 80;
+    this.lightColor = '#f1d9c0';
+    this.darkColor = '#a97a65';
+    this.activeOverlay = '#00cc00';
+    this.previousOverlay = '#cccc00';
+    this.overlayOpacity = 0.4;
+    this.numSquaresToEdge = new Array(64);
+    this.directionOffsets = [8, -8, -1, 1, 7, -7, 9, -9];
+    this.prevMoveSquares = [];
+    this.activeSquare = null;
+    this.hoverSquare = null;
+    this.hoverX = null;
+    this.hoverY = null;
+    this.dragPiece = null;
   }
+
+  init = () => {
+    this.initCanvas();
+    this.initPieces();
+    this.initBoard();
+    this.initEngine();
+    return setInterval(this.draw, 10);
+  };
+
+  initCanvas = () => {
+    const canvas = document.getElementById('canvas');
+    canvas.width = this.squareSize * 8;
+    canvas.height = this.squareSize * 8;
+    canvas.onmousedown = this.onMouseDown;
+    canvas.onmouseup = this.onMouseUp;
+    canvas.onmousemove = this.onMouseMove;
+    canvas.onmouseout = this.onMouseOut;
+    this.ctx = canvas.getContext('2d');
+  };
+
+  initPieces = () => {
+    this.whitePieces = {
+      King: this.getImage('white-king.svg'),
+      Pawn: this.getImage('white-pawn.svg'),
+      Knight: this.getImage('white-knight.svg'),
+      Bishop: this.getImage('white-bishop.svg'),
+      Rook: this.getImage('white-rook.svg'),
+      Queen: this.getImage('white-queen.svg'),
+    };
+    this.blackPieces = {
+      King: this.getImage('black-king.svg'),
+      Pawn: this.getImage('black-pawn.svg'),
+      Knight: this.getImage('black-knight.svg'),
+      Bishop: this.getImage('black-bishop.svg'),
+      Rook: this.getImage('black-rook.svg'),
+      Queen: this.getImage('black-queen.svg'),
+    };
+  };
+
+  initBoard = () => {
+    this.board = new Board(this);
+  };
+
+  initEngine = () => {
+    for (let file = 0; file < 8; file++) {
+      for (let rank = 0; rank < 8; rank++) {
+        const numNorth = 7 - rank;
+        const numSouth = rank;
+        const numWest = file;
+        const numEast = 7 - file;
+
+        const squareIndex = rank * 8 + file;
+
+        this.numSquaresToEdge[squareIndex] = [
+          numNorth,
+          numSouth,
+          numWest,
+          numEast,
+          Math.min(numNorth, numWest),
+          Math.min(numSouth, numEast),
+          Math.min(numNorth, numEast),
+          Math.min(numSouth, numWest),
+        ];
+      }
+    }
+  };
+
+  onMouseDown = (e) => {
+    const square = this.getEventSquare(e);
+    if (square === this.activeSquare) {
+      this.clearActiveSquares()
+    } else if (square.piece) {
+      this.initDrag(square);
+      this.initMove(square);
+      this.setHover(e);
+    }
+  };
+
+  onMouseUp = (e) => {
+    if (!this.activeSquare) return;
+    const square = this.getEventSquare(e);
+    if (this.activeSquare !== square) {
+      this.doMove(square);
+    }
+    if (this.dragPiece) {
+      this.cancelDrag();
+    }
+  };
+
+  onMouseMove = (e) => {
+    this.setHover(e);
+  };
+
+  onMouseOut = () => {
+    this.hoverSquare = null;
+    if (this.dragPiece) {
+      this.cancelDrag();
+      this.activeSquare = null;
+    }
+  };
+
+  setHover = (e) => {
+    this.hoverX = e.offsetX;
+    this.hoverY = e.offsetY;
+    this.hoverSquare = this.activeSquare ? this.getEventSquare(e) : null;
+  }
+
+  initDrag = (fromSquare) => {
+    this.dragPiece = fromSquare.piece;
+  }
+
+  cancelDrag = () => {
+    if (this.activeSquare) {
+      this.activeSquare.piece = this.dragPiece;
+    }
+    this.dragPiece = null;
+  }
+
+  initMove = (fromSquare) => {
+    fromSquare.piece = 0;
+    this.activeSquare = fromSquare;
+  }
+
+  doMove = (toSquare) => {
+    this.prevMoveSquares = [this.activeSquare, toSquare];
+    toSquare.piece = this.dragPiece || this.activeSquare.piece;
+    this.activeSquare.piece = 0;
+    this.clearActiveSquares()
+  }
+
+  clearActiveSquares = () => {
+    this.activeSquare = null;
+    this.hoverSquare = null;
+  }
+
+  getEventSquare = (e) => {
+    const rank = 7 - Math.floor(e.offsetY / this.squareSize);
+    const file = Math.floor(e.offsetX / this.squareSize);
+    return this.board.squares[rank * 8 + file];
+  }
+
+  proportion = (ratio) => Math.floor(this.squareSize * ratio);
+
+  draw = () => {
+    this.board.draw();
+    if (this.dragPiece) {
+      this.drawDragPiece();
+    }
+  }
+
+  drawDragPiece = () => {
+    const img = this.getPieceImage(this.dragPiece);
+    const size = this.proportion(0.8);
+    const x = this.hoverX - (size / 2);
+    const y = this.hoverY - (size / 2);
+    this.ctx.drawImage(img, x, y, size, size);
+  }
+
+  getPieceImage = (piece) => {
+    if (!piece) return null;
+    const set = piece < 16 ? this.whitePieces : this.blackPieces;
+    while (piece >= 8) {
+      piece -= 8;
+    }
+    switch (piece) {
+      case Piece.King: return set.King;
+      case Piece.Pawn: return set.Pawn;
+      case Piece.Knight: return set.Knight;
+      case Piece.Bishop: return set.Bishop;
+      case Piece.Rook: return set.Rook;
+      case Piece.Queen: return set.Queen;
+      default: return null;
+    }
+  }
+
+  getImage = (fileName) => {
+    var img = new Image();
+    img.src = `./images/${fileName}`;
+    return img;
+  };
 }
 
 class Board {
-  constructor(board) {
+  constructor(game) {
+    this.game = game;
     this.squares = new Array(64);
-    if (board instanceof Board) {
-      this.squares = board.squares.map(
-        ({ file, rank, piece }) => new Square(file, rank, piece)
-      );
-    } else {
-      for (let rank = 0; rank < 8; rank++) {
-        for (let file = 0; file < 8; file++) {
-          this.squares[rank * 8 + file] = new Square(file, rank);
-        }
-      }
-      if (typeof board === 'string') {
-        this.loadFen(board);
+    for (let rank = 0; rank < 8; rank++) {
+      for (let file = 0; file < 8; file++) {
+        this.squares[rank * 8 + file] = new Square(file, rank, game);
       }
     }
+    this.loadFen(game.startPosition);
   }
 
   draw = () => {
     this.squares.forEach(sq => sq.draw());
   };
+
+  isDigit = (str) => /^\d+$/.test(str);
 
   loadFen = (fen) => {
     const pieceTypeFromSymbol = {
@@ -137,7 +242,7 @@ class Board {
       if (symbol === '/') {
         file = 0;
         rank--;
-      } else if (isDigit(symbol)) {
+      } else if (this.isDigit(symbol)) {
         file += parseInt(symbol);
       } else {
         const pieceColor = symbol === symbol.toUpperCase() ? Piece.White : Piece.Black;
@@ -150,21 +255,22 @@ class Board {
 }
 
 class Square {
-  constructor(file, rank, piece) {
+  constructor(file, rank, game) {
     this.file = file;
     this.rank = rank;
-    this.piece = piece || 0;
+    this.game = game;
+    this.piece = 0;
     this.isLightSquare = (file + rank) % 2 === 0;
   }
 
-  get squareColor() { return this.isLightSquare ? _lightColor : _darkColor; }
-  get textColor() { return this.isLightSquare ? _darkColor : _lightColor; }
-  get xPos() { return this.file * _squareSize; }
-  get yPos() { return (_squareSize * 7) - (this.rank * _squareSize); }
+  get squareColor() { return this.isLightSquare ? this.game.lightColor : this.game.darkColor; }
+  get textColor() { return this.isLightSquare ? this.game.darkColor : this.game.lightColor; }
+  get xPos() { return this.file * this.game.squareSize; }
+  get yPos() { return (this.game.squareSize * 7) - (this.rank * this.game.squareSize); }
 
   draw = () => {
-    _ctx.fillStyle = this.squareColor;
-    _ctx.fillRect(this.xPos, this.yPos, _squareSize, _squareSize);
+    this.game.ctx.fillStyle = this.squareColor;
+    this.game.ctx.fillRect(this.xPos, this.yPos, this.game.squareSize, this.game.squareSize);
 
     if (this.file === 0) {
       this.drawRankLabel();
@@ -174,12 +280,12 @@ class Square {
       this.drawFileLabel();
     }
 
-    if ([_activeSquare, _hoverSquare].includes(this)) {
-      this.drawOverlay(_activeOverlay);
+    if ([this.game.activeSquare, this.game.hoverSquare].includes(this)) {
+      this.drawOverlay(this.game.activeOverlay);
     }
 
-    if (_prevMoveSquares.includes(this)) {
-      this.drawOverlay(_previousOverlay);
+    if (this.game.prevMoveSquares.includes(this)) {
+      this.drawOverlay(this.game.previousOverlay);
     }
 
     if (this.piece) {
@@ -189,36 +295,36 @@ class Square {
 
   drawRankLabel = () => {
     const rankText = `${this.rank + 1}`;
-    const x = this.xPos + proportion(0.05);
-    const y = this.yPos + proportion(0.2);
-    _ctx.fillStyle = this.textColor;
-    _ctx.font = `400 ${proportion(0.175)}px sans-serif`;
-    _ctx.fillText(rankText, x, y);
+    const x = this.xPos + this.game.proportion(0.05);
+    const y = this.yPos + this.game.proportion(0.2);
+    this.game.ctx.fillStyle = this.textColor;
+    this.game.ctx.font = `400 ${this.game.proportion(0.175)}px sans-serif`;
+    this.game.ctx.fillText(rankText, x, y);
   };
 
   drawFileLabel = () => {
     const fileText = 'abcdefgh'[this.file];
-    const x = this.xPos + _squareSize - proportion(0.15);
-    const y = this.yPos + _squareSize - proportion(0.075);
-    _ctx.fillStyle = this.textColor;
-    _ctx.font = `400 ${proportion(0.175)}px sans-serif`;
-    _ctx.fillText(fileText, x, y);
+    const x = this.xPos + this.game.squareSize - this.game.proportion(0.15);
+    const y = this.yPos + this.game.squareSize - this.game.proportion(0.075);
+    this.game.ctx.fillStyle = this.textColor;
+    this.game.ctx.font = `400 ${this.game.proportion(0.175)}px sans-serif`;
+    this.game.ctx.fillText(fileText, x, y);
   };
 
   drawOverlay = (color) => {
-    _ctx.fillStyle = color;
-    _ctx.globalAlpha = _overlayOpacity;
-    _ctx.fillRect(this.xPos, this.yPos, _squareSize, _squareSize);
-    _ctx.globalAlpha = 1.0;
+    this.game.ctx.fillStyle = color;
+    this.game.ctx.globalAlpha = this.game.overlayOpacity;
+    this.game.ctx.fillRect(this.xPos, this.yPos, this.game.squareSize, this.game.squareSize);
+    this.game.ctx.globalAlpha = 1.0;
   }
 
   drawPiece = () => {
-    const img = getPieceImage(this.piece);
-    const offset = proportion(0.1);
-    const size = proportion(0.8);
+    const img = this.game.getPieceImage(this.piece);
+    const offset = this.game.proportion(0.1);
+    const size = this.game.proportion(0.8);
     const x = this.xPos + offset;
     const y = this.yPos + offset;
-    _ctx.drawImage(img, x, y, size, size);
+    this.game.ctx.drawImage(img, x, y, size, size);
   };
 }
 
@@ -234,129 +340,3 @@ class Piece {
   static White = 8;
   static Black = 16;
 }
-
-// ------------------------------------
-// helper functions
-// ------------------------------------
-const proportion = (ratio) => Math.floor(_squareSize * ratio);
-
-const isDigit = (str) => /^\d+$/.test(str);
-
-// ------------------------------------
-// move functions
-// ------------------------------------
-function onMouseDown(e) {
-  const square = getEventSquare(e);
-  if (square === _activeSquare) {
-    clearActiveSquares()
-  } else if (square.piece) {
-    initDrag(square);
-    initMove(square);
-    setHover(e);
-  }
-}
-
-function onMouseUp(e) {
-  if (!_activeSquare) return;
-  const square = getEventSquare(e);
-  if (_activeSquare !== square) {
-    doMove(square);
-  }
-  if (_dragPiece) {
-    cancelDrag();
-  }
-}
-
-function onMouseMove(e) {
-  setHover(e);
-}
-
-function onMouseOut() {
-  _hoverSquare = null;
-  if (_dragPiece) {
-    cancelDrag();
-    _activeSquare = null;
-  }
-}
-
-function setHover(e) {
-  _hoverX = e.offsetX;
-  _hoverY = e.offsetY;
-  _hoverSquare = _activeSquare ? getEventSquare(e) : null;
-}
-
-function initDrag(fromSquare) {
-  _dragPiece = fromSquare.piece;
-}
-
-function cancelDrag() {
-  if (_activeSquare) {
-    _activeSquare.piece = _dragPiece;
-  }
-  _dragPiece = null;
-}
-
-function initMove(fromSquare) {
-  fromSquare.piece = 0;
-  _activeSquare = fromSquare;
-}
-
-function doMove(toSquare) {
-  _prevMoveSquares = [_activeSquare, toSquare];
-  toSquare.piece = _dragPiece || _activeSquare.piece;
-  _activeSquare.piece = 0;
-  clearActiveSquares()
-}
-
-function clearActiveSquares() {
-  _activeSquare = null;
-  _hoverSquare = null;
-}
-
-function getEventSquare(e) {
-  const rank = 7 - Math.floor(e.offsetY / _squareSize);
-  const file = Math.floor(e.offsetX / _squareSize);
-  return _board.squares[rank * 8 + file];
-}
-
-// ------------------------------------
-// draw functions
-// ------------------------------------
-function draw() {
-  _board.draw();
-  if (_dragPiece) {
-    drawDragPiece();
-  }
-}
-
-function drawDragPiece() {
-  const img = getPieceImage(_dragPiece);
-  const size = proportion(0.8);
-  const x = _hoverX - (size / 2);
-  const y = _hoverY - (size / 2);
-  _ctx.drawImage(img, x, y, size, size);
-}
-
-const getPieceImage = (piece) => {
-  if (!piece) return null;
-  const set = piece < 16 ? _whitePieces : _blackPieces;
-  while (piece >= 8) {
-    piece -= 8;
-  }
-  switch (piece) {
-    case Piece.King: return set.King;
-    case Piece.Pawn: return set.Pawn;
-    case Piece.Knight: return set.Knight;
-    case Piece.Bishop: return set.Bishop;
-    case Piece.Rook: return set.Rook;
-    case Piece.Queen: return set.Queen;
-    default: return null;
-  }
-}
-
-// BUGS:
-/*
-Steps: click on a piece, click it again
-Expected Behavior: it should deselect the square as active
-Actual Behavior: it removes the piece and square remains active
-*/
