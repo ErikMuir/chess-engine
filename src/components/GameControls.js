0e63c0ee8b71db4b0958c38375589d4e0eba2116
Erik Muir <erikdmuir@gmail.com>
2021-12-20 23:08:32 -0600

    refactor game controls

--- src/components/GameControls.js
+++ src/components/GameControls.js
@@ -13,28 +13,23 @@ import Logger from '../Logger';
 const logger = new Logger('Controls');
 const loadGameInputId = 'load-game-input';
 
-class GameControls extends React.Component {
-  constructor(props) {
-    super(props);
-    logger.trace('ctor');
-    this.fileReader = new FileReader();
-    this.fileReader.onerror = () => logger.error(this.fileReader.error);
-    this.fileReader.onload = () => this.fileReaderOnLoad(this.fileReader.result);
-  }
+const GameControls = (props) => {
+  logger.trace('render');
 
-  readFile = () => {
-    logger.trace('readFile');
-    const loadGameInput = document.getElementById(loadGameInputId);
-    const file = loadGameInput.files[0];
-    this.fileReader.readAsText(file);
-  };
+  const {
+    importGame,
+    newGame,
+    exportGame,
+    resign,
+  } = props;
 
-  fileReaderOnLoad = (readerResult) => {
+  const fileReader = new FileReader();
+  fileReader.onerror = () => logger.error(fileReader.error);
+  fileReader.onload = () => {
     logger.trace('fileReaderOnLoad');
     try {
-      const rawJson = JSON.parse(readerResult);
+      const rawJson = JSON.parse(fileReader.result);
       const gameJson = getStandardizedGameJson(rawJson);
-      const { importGame } = this.props;
       importGame(gameJson);
     } catch (err) {
       logger.error('Error loading game', err);
@@ -42,62 +37,63 @@ class GameControls extends React.Component {
     }
   };
 
-  handleNew = (e) => {
+  const readFile = () => {
+    logger.trace('readFile');
+    const loadGameInput = document.getElementById(loadGameInputId);
+    const file = loadGameInput.files[0];
+    fileReader.readAsText(file);
+  };
+
+  const handleNew = (e) => {
     logger.trace('handleNew');
-    const { newGame } = this.props;
     newGame();
     e.currentTarget.blur();
   };
 
-  handleImport = (e) => {
+  const handleImport = (e) => {
     logger.trace('handleImport');
     const loadGameInput = document.getElementById(loadGameInputId);
     loadGameInput.click();
     e.currentTarget.blur();
   };
 
-  handleExport = (e) => {
+  const handleExport = (e) => {
     logger.trace('handleExport');
-    const { exportGame } = this.props;
     exportGame();
     e.currentTarget.blur();
   };
 
-  handleResign = (e) => {
+  const handleResign = (e) => {
     logger.trace('handleResign');
-    const { resign } = this.props;
     resign();
     e.currentTarget.blur();
   };
 
-  render() {
-    logger.trace('render');
-    return (
-      <div className="aside game-controls" style={{ height: boardSize }}>
-        <div>
-          <button type="button" onClick={this.handleNew} title="New Game">
-            <Icon path={mdiNewBox} size={1.5} color="#eeeeee" />
-          </button>
-        </div>
-        <div>
-          <button type="button" onClick={this.handleImport} title="Import">
-            <Icon path={mdiImport} size={1.5} color="#eeeeee" />
-          </button>
-        </div>
-        <div>
-          <button type="button" onClick={this.handleExport} title="Export">
-            <Icon path={mdiTrayArrowDown} size={1.5} color="#eeeeee" />
-          </button>
-        </div>
-        <div>
-          <button type="button" onClick={this.handleResign} title="Resign">
-            <Icon path={mdiFlag} size={1.5} color="#eeeeee" />
-          </button>
-        </div>
-        <input type="file" id={loadGameInputId} onChange={this.readFile} accept="application/json" />
+  return (
+    <div className="aside game-controls" style={{ height: boardSize }}>
+      <div>
+        <button type="button" onClick={handleNew} title="New Game">
+          <Icon path={mdiNewBox} size={1.5} color="#eeeeee" />
+        </button>
       </div>
-    );
-  }
-}
+      <div>
+        <button type="button" onClick={handleImport} title="Import">
+          <Icon path={mdiImport} size={1.5} color="#eeeeee" />
+        </button>
+      </div>
+      <div>
+        <button type="button" onClick={handleExport} title="Export">
+          <Icon path={mdiTrayArrowDown} size={1.5} color="#eeeeee" />
+        </button>
+      </div>
+      <div>
+        <button type="button" onClick={handleResign} title="Resign">
+          <Icon path={mdiFlag} size={1.5} color="#eeeeee" />
+        </button>
+      </div>
+      <input type="file" id={loadGameInputId} onChange={readFile} accept="application/json" />
+    </div>
+  );
+};
 
 export default GameControls;
